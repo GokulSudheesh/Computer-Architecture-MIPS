00c1e808ac3e62917c125d454412d7d4d323ae99
Gokul Sudheesh <38152409+GokulSudheesh@users.noreply.github.com>
2020-11-02 13:38:40 +0400

    Update L5_arrayTraversalMethod2.s

--- Week 5/L5_arrayTraversalMethod2.s
+++ Week 5/L5_arrayTraversalMethod2.s
@@ -6,12 +6,12 @@ space: .asciiz " "
 
 main:
 	li $t1, 0 #i=0
-	li $t2, 0 #i*4
+	li $t2, 0 #Array Counter
 	lw $t0, length
 
 loop:
 	bge $t1, $t0, exit
-	sll $t2, $t1, 2 #shifting 2 times is like multiplying by 4
+	sll $t2, $t1, 2 #i*4
 	lw $t3, array($t2)
 
 	move $a0, $t3
@@ -22,7 +22,6 @@ loop:
     la $a0, space # argument: string
 	syscall 
 	addi $t1, $t1, 1
-	addi $t2, $t2, 4
 	j loop
 exit:
 	li $v0, 10
